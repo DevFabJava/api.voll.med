200e42c7eb1f4a3f682894c9714693edcc395570
DevFabJava <dev.fabioalmeida@proton.me>
2025-06-26 20:43:49 -0300

    Atualizando o código

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -40,4 +40,17 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
 
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedicos dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 }
